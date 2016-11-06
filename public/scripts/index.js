76749b69241e906ecf8e0d65e1a51b25b77ad07b
mavnuz22 <mavnuz94@gmail.com>
2016-11-06 15:08:32 +0100

    Added onclick listeners for buttons + load current and past programs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $.ajax({
         type: 'GET',
-        url: '/api/getList',
+        url: '/api/workoutPlan',
         contentType: 'application/json; charset=utf-8',
-        data: data,
         timeout: 3000,
-        success: function(data, textStatus, jQxhr) {
+        success: function(data) {
             var currentPrograms = $('#currentPrograms');
-            //var pastPrograms = $('#pastPrograms')
-
-            data.forEach(function(element) {
+            var pastPrograms = $('#pastPrograms')
+            var json = JSON.parse(data);
+            
+            json.currentPlans.forEach(function(element) {
                 var opt = document.createElement('option');
                 opt.value = element.id;
                 opt.innerHTML = element.name;
@@ -17,47 +17,42 @@ $(document).ready(function() {
                 currentPrograms.append(opt);
             }, this);
             
-            /*for (var i = 0; i < 3; i++){
+            json.pastPlans.forEach(function(element) {
                 var opt = document.createElement('option');
-                var opt2 = document.createElement('option');
-                opt.value = i;
-                opt2.value = i;
+                opt.value = element.id;
+                opt.innerHTML = element.name;
                 
-                opt.innerHTML = "Test " + i;
-                opt2.innerHTML = "PastTest " + i;
-                
-                currentPrograms.append(opt);
-                pastPrograms.append(opt2);
-            }*/
+                pastPrograms.append(opt);
+            }, this);
         },
         error: function(jqXhr, textStatus, errorThrown) {
             alert('An error occured: ' + errorThrown);
         }
     });
 
-    
-});
-
-$('#currentPrograms').on('change', function() {
-    $.ajax({
-        type: 'GET',
-        url: '/api/getById' + this.value, //Hopefully value of plan?
-        contentType: 'application/json; charset=utf-8',
-        data: data,
-        timeout: 3000,
-        success: function(data, textStatus, jQxhr) {
-            //TODO load data into table
-        },
-        error: function(jqXhr, textStatus, errorThrown) {
-            alert('An error occured: ' + errorThrown);
-        }
+    $('#currentPrograms').click(function() {
+        $.ajax({
+            type: 'GET',
+            url: '/api/workoutPlan/' + this.value, //Hopefully value of plan?
+            contentType: 'application/json; charset=utf-8',
+            timeout: 3000,
+            success: function(data) {
+                //TODO load data into table
+                console.log(data);
+            },
+            error: function(jqXhr, textStatus, errorThrown) {
+                alert('An error occured: ' + errorThrown);
+            }
+        });
     });
-});
 
-$('#createProgram').click(function () {
-    return;
-});
+    $('#createProgram').click(function () {
+        console.log("Create clicked");
+        window.location.replace("create");
+    });
 
-$('#logProgram').click(function () {
-    return;
+    $('#logProgram').click(function () {
+        return;
+    });
+
 });
